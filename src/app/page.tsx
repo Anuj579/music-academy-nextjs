bc9313b43f36aa2f1e99dd340c2c41edb21ae2b1
Anuj Chaudhary <alwaysmile9813@gmail.com>
2024-09-23 10:00:24 +0530

    Home page final update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
 import TestimonialCards from "@/components/TestimonialCards";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
@@ -7,11 +9,13 @@ import WhyChooseUs from "@/components/WhyChooseUs";
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <HeroSection/>
-      <FeaturedCourses/>
-      <WhyChooseUs/>
-      <TestimonialCards/>
-      <UpcomingWebinars/>
+      <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs />
+      <TestimonialCards />
+      <UpcomingWebinars />
+      <Instructors />
+      <Footer />
     </main>
   );
 }
